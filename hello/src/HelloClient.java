28d461b80c10fa45993e5a64d482399de9e6cbe8
Sophal24 <sophalisco23@gmail.com>
2022-02-25 16:14:37 +0100

    combined 5.2 to 5.1

--- hello/src/HelloClient.java
+++ hello/src/HelloClient.java
@@ -14,38 +14,38 @@ public class HelloClient implements Info_itf {
 	}
 
 	// Methods
-public String getName() throws RemoteException {
-	return name;
-}
-
-  public static void main(String [] args) {
-	
-	try {
-	  if (args.length < 1) {
-	   System.out.println("Usage: java HelloClient <rmiregistry host>");
-	   return;}
-
-	String host = args[0];
-
-	// Get remote object reference
-	Registry registry = LocateRegistry.getRegistry(host); 
-	System.out.println(registry);
-	Hello h = (Hello) registry.lookup("HelloService");
-
-	// Remote method invocation
-	// Scanner input = new Scanner(System.in);
-	// System.out.print("Input Client Name: ");  
-	// String clientName = input.nextLine();
-
-	Info_itf getClientName = new HelloClient("Tony");
-	Info_itf clientName = (Info_itf) UnicastRemoteObject.exportObject(getClientName, 0);
-
-	String res = h.sayHello(clientName);
-	System.out.println(res);
-	// input.close();
-
-	} catch (Exception e)  {
-		System.err.println("Error on client: " + e);
+	public String getName() throws RemoteException {
+		return name;
 	}
-  }
+
+	public static void main(String[] args) {
+
+		try {
+			if (args.length < 1) {
+				System.out.println("Usage: java HelloClient <rmiregistry host>");
+				return;
+			}
+
+			String host = args[0];
+
+			// Get remote object reference
+			Registry registry = LocateRegistry.getRegistry(host);
+			Hello h = (Hello) registry.lookup("HelloService");
+
+			// Remote method invocation
+			Scanner input = new Scanner(System.in);
+			System.out.print("Input your name and Enter: ");
+			String name = input.nextLine();
+			input.close();
+
+			Info_itf getClientName = new HelloClient(name);
+			Info_itf clientName = (Info_itf) UnicastRemoteObject.exportObject(getClientName, 0);
+
+			String res = h.sayHello(clientName);
+			System.out.println(res);
+
+		} catch (Exception e) {
+			System.err.println("Error on client: " + e);
+		}
+	}
 }
